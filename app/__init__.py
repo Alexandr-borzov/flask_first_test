f7219abff5cb2133e2c4b7d3cf4e316e56c5189c
Александр <aleks-40k@mail.ru>
2024-04-04 22:50:15 +0500

    v 1.1

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ from app.registration.registration import registration
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///metrology.db'
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['SECRET_KEY'] = 'fdgfh78@#5?>gfhf89dx,v06k'
 
     db.init_app(app)
     manager.init_app(app)
